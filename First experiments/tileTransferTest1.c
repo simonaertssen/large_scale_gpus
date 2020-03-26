4d0ffe294fde415b98fb9293a3c761d2a312e169
Louis Hein <hein.louis@gmail.com>
2020-03-26 17:52:38 +0100

    Code cleanup, zero-row bug still unsolved.

--- First experiments/tileTransferTest1.c
+++ First experiments/tileTransferTest1.c
@@ -7,13 +7,9 @@
 // nvcc -lcublas ../DIEKUHDA/kuhda.c tileTransferTest1.c && ./a.out
 
 int main(){
-	// Set verbose to 0 to mute output
-	int verbose = 0;
 
 	unsigned long n = 8, size = n * n * sizeof(double);
 	int x = n/2, sizex = x * x * sizeof(double); // x * x = dimension of quarter tile
-
-	printf("n = %d\n First tile expected output: %d x %d identity matrix.\n", n, x, x);
 
 	// Containers for host and device matrices
 	matrix *h_A = kuhdaMallocMdiag(n, n); // full size A matrix
@@ -23,18 +19,24 @@ int main(){
 	gpuErrchk(cudaSetDevice(0));
 	matrix *d_A1 = kuhdaMatrixToGPU(x, x, h_A);
 
-	// ...retrieve it again
-	if (verbose == 1) printf("Transfer %d to h: ", 0);
+	printf("n = %d\nThe full matrix is:\n", n);
+	kuhdaPrintM(h_A);
 
+	// ...retrieve it again
+
+	/* 
 	cudaStream_t *stream;
 	stream = (cudaStream_t *) malloc(sizeof(cudaStream_t));
 	gpuErrchk(cudaStreamCreate(&stream));
-
 	// inputs: (void *dst, const void *src, size_t count, enum cudaMemcpyKind	kind, cudaStream_t stream)
 	gpuErrchk(cudaMemcpy(h_A1->data, d_A1->data, sizex, cudaMemcpyDeviceToHost, stream));
+	*/
 
+	// cleaner way:
+	kuhdaMatrixToHost(x, x, d_A1, h_A1);
 
-	// print retrieved matrix (should be diagonal 4x4)
+	// print retrieved matrix
+	printf("\nFirst tile expected output: %d x %d identity matrix.\n", x, x);
 	kuhdaPrintM(h_A1);
 
 }
