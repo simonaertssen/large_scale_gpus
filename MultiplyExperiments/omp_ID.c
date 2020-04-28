a70a9524f713e56c1134ce26d0ee63ea7227589c
Louis Hein <hein.louis@gmail.com>
2020-04-28 16:24:15 +0200

    OMP id test

---
+++ MultiplyExperiments/omp_ID.c
@@ -0,0 +1,23 @@
+#include <omp.h>
+#include <stdio.h>
+
+
+ int HelloFunc()
+{
+    int i;
+    int numthreads = 8;
+#pragma omp parallel for default(none) num_threads(numthreads) private(i)
+    for (i = 0; i < 10; i++)
+    {
+        int tid = omp_get_thread_num();
+        printf("Hello world from omp thread %d\n", tid);
+    }
+    return -1;
+}
+
+int main()
+{
+    HelloFunc();
+
+  return 0;
+}
