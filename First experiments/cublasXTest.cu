790ab1433ac5bf1ae330a02119bfba0da5dc6f3c
Simon Joris A Aertssen <simonaertssen@gmail.com>
2020-05-05 19:13:57 +0200

    Simon wanted something back on his laptop

--- First experiments/cublasXTest.cu
+++ First experiments/cublasXTest.cu
@@ -11,7 +11,7 @@ int main() {
     int device_count, status = 0;
     gpuErrchk(cudaGetDeviceCount(&device_count));
 
-	unsigned long n = 16384, m = n, k = n;
+	unsigned long n = 16384*2, m = n, k = n;
 
     // Containers for host and device matrices:
     printf("Allocating devices\n");
@@ -19,9 +19,9 @@ int main() {
 	matrix *h_B  = kuhdaMallocMP1(n, n); // diagonal B matrix
 	matrix *h_C  = kuhdaMallocMP(n, n); // empty C matrix
 
-	matrix *d_A  = kuhdaMallocDeviceM(n, n); 
-	matrix *d_B  = kuhdaMallocDeviceM(n, n);
-	matrix *d_C  = kuhdaMallocDeviceM(n, n);
+	// matrix *d_A  = kuhdaMallocDeviceM(n, n); 
+	// matrix *d_B  = kuhdaMallocDeviceM(n, n);
+	// matrix *d_C  = kuhdaMallocDeviceM(n, n);
 
 	// Make streams and copy data:
 	cudaStream_t mainstream, copystream1, copystream2;
@@ -48,15 +48,21 @@ int main() {
     if (status != 0) {
         fprintf(stderr, "!!!! CUBLASXT handle creation error\n");
         return -1;
-        }
+		}
+	
     int devices[4] = {0, 1, 2, 3};
     status = cublasXtDeviceSelect(Xt_handle, device_count, devices);
     if (status != 0) {
         fprintf(stderr, "!!!! CUBLASXT device selection error\n");
         return -1;
         }
-
-      double alpha = 1.0, beta  = 0.0;
+	status = cublasXtSetBlockDim(Xt_handle, 32);
+	if (status != 0) {
+        fprintf(stderr, "!!!! CUBLASXT block dim selection error\n");
+        return -1;
+        }
+	
+	double alpha = 1.0, beta  = 0.0;
     gpuErrchk(cudaEventRecord(mainstart, mainstream));
     status = cublasXtDgemm(Xt_handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, &alpha,
         h_A->data, m, h_B->data, k, &beta, h_C->data, m);
@@ -70,6 +76,10 @@ int main() {
     gpuErrchk(cudaEventSynchronize(mainstop));
     gpuErrchk(cudaEventElapsedTime(&mainstreamtimer, mainstart, mainstop));
 	printf("Multiplication took %lf seconds\n", mainstreamtimer/1000);
+	long int numerator    = (long int)(m * n) * (2 * ((long long)k) + 2);
+	long long denominator = 1.0e6 * mainstreamtimer;
+	long long gflops = numerator / denominator;
+	printf("%lu GFLPS\n", gflops);
 	
 	gpuErrchk(cudaDeviceSynchronize());
 	
@@ -95,8 +105,8 @@ int main() {
 	kuhdaFreeM(h_A, 'p');
 	kuhdaFreeM(h_B, 'p');
 	kuhdaFreeM(h_C, 'p');
-	kuhdaFreeM(d_A, 'c');
-	kuhdaFreeM(d_B, 'c');
-	kuhdaFreeM(d_C, 'c');
+	// kuhdaFreeM(d_A, 'c');
+	// kuhdaFreeM(d_B, 'c');
+	// kuhdaFreeM(d_C, 'c');
 	return 0;
 }
