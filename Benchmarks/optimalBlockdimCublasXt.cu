63a3fabe7b9d65afd659a03c6b9b5116fa59cd1a
Simon Aertssen <Simonaertssen@gmail.com>
2020-05-05 20:05:06 +0200

    added benchmark folder for final tests on cluster and added p9 logon to txt file

---
+++ Benchmarks/optimalBlockdimCublasXt.cu
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "../DIEKUHDA/kuhda.h"
+#include "cuda.h"
+
+// With this script, we aim to measure the performance of cublasXt on the DTU cluster for different block dimensions.
+// Run with nvcc -O3 -lcublas ../DIEKUHDA/kuhda.cu optimalBlockdimCublasXt.cu && ./a.out
+
+int main() {
+	
+
+	return 0;
+}
